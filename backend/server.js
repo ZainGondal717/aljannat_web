fb844c5476dccdc412f26b0b1d4c72860e92f8e0
soul <soul@soul.soul>
2025-07-30 17:42:16 +0500

    chnage in env2

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 'https://aljannat-web.vercel.app/';
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
